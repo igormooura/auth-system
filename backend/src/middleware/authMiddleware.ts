dbee7daca84b0cbce4149a9cf432d58598f403f8
igormooura <igor.oliveira.moura@gmail.com>
2025-03-21 16:36:33 -0300

    fix(authMiddleware): fixed token verification logic

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -19,11 +19,14 @@ const handleUnauthorizedError = (next: NextFunction): void => {
 function authMiddleware(req: Request, res: Response, next: NextFunction) {
     const authHeader = req.get('Authorization') // get authHeader -> check if it's valid 
 
+    
     if (authHeader) {
         const bearer = authHeader.split(' ')[0].toLowerCase();
         const token = authHeader.split(' ')[1];
+        console.log("Bearer:", bearer); // Log para verificar o prefixo
+        console.log("Token:", token);
 
-        if (bearer && token === 'bearer') {
+        if (token && bearer === 'bearer') {
             const decode = jwt.verify(token, 'segredo_demais');
 
             if (decode) {
