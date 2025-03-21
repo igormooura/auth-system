73e1a8aaad5f384ab733e0c0feaa6e66392ea4fe
Igor Moura <igor.oliveira.moura@gmail.com>
2025-03-20 21:23:21 -0300

    style: created access controller

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from "../controller/userController"; 
+import { deleteUser, getAllUsers, getUserById, updateUser } from "../controller/userController"; 
+
 
 const router = express.Router();
 
-router.post("/register", createUser);
 router.get("/users", getAllUsers);
 router.get("/user/:_id", getUserById);
 router.put("/user/:_id", updateUser);
