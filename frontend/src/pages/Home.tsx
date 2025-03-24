4f0a131d80c35191dd64793053bd348fb57e34cc
Igor Moura <igor.oliveira.moura@gmail.com>
2025-03-23 21:46:02 -0300

    feat: added HomePage

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import DeleteButton from "../components/Buttons/DeleteButton";
+import SubmitButton from "../components/Buttons/SubmitButton";
+
+const Home = () => {
+  const handleDeleteAccount = () => {
+ 
+    console.log("Conta deletada!");
+    alert("Conta deletada com sucesso!");
+  };
+
+  return (
+    <div className="bg-gradient-to-tl from-cyan-900 to-cyan-500 min-h-screen flex items-center justify-center p-4">
+    
+    <SubmitButton> Edit Info </SubmitButton>
+
+      <DeleteButton onClick={handleDeleteAccount}>Delete account</DeleteButton>
+    </div>
+  );
+};
+
+export default Home;
