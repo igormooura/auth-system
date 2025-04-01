f308b04c6a40735da39d7ad883115d9621dc93d7
igormooura <igor.oliveira.moura@gmail.com>
2025-04-01 16:58:27 -0300

    feat: Title EditProfile

---
+++ frontend/src/pages/Edit.tsx
@@ -0,0 +1,15 @@
+
+const Edit = () => {
+    return(
+        <div className="bg-gradient-to-tl from-cyan-900 to-cyan-500 min-h-screen  p-4">
+        
+        <div className="flex justify-center">
+            <h1 className="text-white text-2xl font-semibold"> Edit Profile</h1>
+        </div>
+            
+            
+        </div>
+    )
+}
+
+export default Edit;
