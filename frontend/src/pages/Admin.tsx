76a4826cd219bbc1f64fb79d195bc549fe3fe0d8
Igor Moura <igor.oliveira.moura@gmail.com>
2025-04-18 21:28:54 -0300

    feat: added delete function to table

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { TiDelete } from "react-icons/ti";
 
 type User = {
   _id: string;
@@ -17,23 +18,39 @@ const Admin = () => {
         const response = await axios.get("http://localhost:4000/users");
         setUsers(response.data);
       } catch (error) {
-        console.error("Erro ao buscar usuários:", error);
+        console.error("Error:", error);
       }
     };
 
     fetchUsers();
   }, []);
 
+  const handleDeleteAccount = async (_id: string) => {
+    const confirmDelete = window.confirm(
+      "Do you really want to delete this account?"
+    );
+
+    if (!confirmDelete) return;
+
+    try {
+      const response = await axios.delete(`http://localhost:4000/user/${_id}`);
+      console.log("Usuário deletado com sucesso:", response.data);
+    } catch (error) {
+      console.error("Erro ao deletar usuário:", error);
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center bg-gradient-to-tl from-cyan-900 to-cyan-500 min-h-screen p-4">
-      <h1 className="text-white text-2xl font-bold mb-6">Usuários</h1>
+      <h1 className="text-white text-2xl font-bold mb-6">Users</h1>
       <div className="bg-white rounded-lg overflow-hidden shadow-md w-full max-w-4xl">
         <table className="min-w-full text-sm text-left">
           <thead className="bg-cyan-800 text-white">
             <tr>
-              <th className="px-6 py-3">Nome</th>
+              <th className="px-6 py-3">Name</th>
               <th className="px-6 py-3">Email</th>
               <th className="px-6 py-3">Admin</th>
+              <th className="px-6 py-3"> <TiDelete/></th>
             </tr>
           </thead>
           <tbody className="bg-white text-gray-700">
@@ -46,13 +63,23 @@ const Admin = () => {
                 <td className="px-6 py-4">{user.email}</td>
                 <td className="px-6 py-4">
                   {user.isAdmin ? (
-                    <span className="text-green-600 font-semibold">Sim</span>
+                    <span className="text-green-600 font-semibold">Yes</span>
                   ) : (
-                    <span className="text-red-600 font-semibold">Não</span>
+                    <span className="text-red-600 font-semibold">No</span>
                   )}
                 </td>
+                <td className="px-6 py-4">
+              <button
+                onClick={() => handleDeleteAccount(user._id)}
+              >
+              <TiDelete />
+
+              </button>
+            </td>
               </tr>
+              
             ))}
+            
           </tbody>
         </table>
       </div>
