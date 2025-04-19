9354aae0ffe2008f27c3d2c92d90df8b0c90de79
Igor Moura <igor.oliveira.moura@gmail.com>
2025-04-18 21:39:43 -0300

    feat: added framer motion to home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,6 +5,7 @@ import SubmitButton from "../components/Buttons/SubmitButton";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import LeaveButton from "../components/Buttons/LeaveButton";
+import { motion } from "framer-motion";
 
 const Home: React.FC = () => {
   const { userInfo, token, logout } = useVerifyAuth();
@@ -48,24 +49,51 @@ const Home: React.FC = () => {
 
   return (
     <div className="bg-gradient-to-tl from-cyan-900 to-cyan-500 min-h-screen w-full flex flex-col justify-between items-center p-52">
-      <div className="flex flex-col items-center gap-6 mt-20">
-        <div className="w-48">
+      <motion.div
+        className="flex flex-col items-center gap-6 mt-20"
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.7, delay: 0.2 }}
+      >
+        <motion.div
+          className="w-48"
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+        >
           <SubmitButton onClick={handleEditUser}>Edit Info</SubmitButton>
-        </div>
+        </motion.div>
 
-        <div className="flex flex-row gap-10">
-          <div className="w-40">
+        <motion.div
+          className="flex flex-row gap-10"
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.7, delay: 0.4 }}
+        >
+          <motion.div
+            className="w-40"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
             <DeleteButton onClick={handleDeleteAccount}>
               Delete account
             </DeleteButton>
-          </div>
-          <div className="w-40">
+          </motion.div>
+          <motion.div
+            className="w-40"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
             <LeaveButton onClick={handleLeave}>Leave</LeaveButton>
-          </div>
-        </div>
-      </div>
+          </motion.div>
+        </motion.div>
+      </motion.div>
 
-      <div className="w-full flex justify-center items-center h-24">
+      <motion.div
+        className="w-full flex justify-center items-center h-24"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: 0.8 }}
+      >
         {userInfo?.isAdmin && (
           <div className="text-white font-bold text-xl text-center">
             Admin resource:
@@ -76,7 +104,7 @@ const Home: React.FC = () => {
             </p>
           </div>
         )}
-      </div>
+      </motion.div>
     </div>
   );
 };
