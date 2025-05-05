47db1e59b614d6f093d6e89e45c469cdb0e0965b
igormooura <igor.oliveira.moura@gmail.com>
2025-05-05 16:03:32 -0300

    feat: deploy cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,35 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://auth-system-olive.vercel.app'
+  ];
+  
+  app.use(cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+  
+  app.options('*', cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true
+  }));
+  
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
