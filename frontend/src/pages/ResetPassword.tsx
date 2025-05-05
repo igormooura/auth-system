c23c73cfb2c5763fb39efc37cd0fcb64c184739f
igormooura <igor.oliveira.moura@gmail.com>
2025-05-05 15:03:59 -0300

    feat: added feedback messages

--- frontend/src/pages/ResetPassword.tsx
+++ frontend/src/pages/ResetPassword.tsx
@@ -37,10 +37,10 @@ const ResetPassword = () => {
       );
 
       if (response.status === 200) {
-        
-          alert("Password successfully reseted.")
+        setSuccess(true);
+        setTimeout(() => {
           navigate("/");
-  
+        }, 2000);
       }
     } catch (error) {
       if (axios.isAxiosError(error)) {
@@ -79,6 +79,18 @@ const ResetPassword = () => {
           >
             Reset Password
           </button>
+          
+          {error && (
+            <p className="mt-2 text-red-700 bg-red-100 border border-red-400 px-4 py-2 rounded">
+              {error}
+            </p>
+          )}
+
+          {success && (
+            <p className="mt-2 text-green-700 bg-green-100 border border-green-400 px-4 py-2 rounded">
+              Password successfully reset.
+            </p>
+          )}
         </form>
       </div>
     </div>
